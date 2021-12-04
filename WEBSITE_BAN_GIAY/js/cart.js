cd2bee8e4a69c567c3bb279fc1c1c48f5f1cdea4
patuan0810 <patuan0810@gmail.com>
2021-12-04 15:13:33 +0700

    first commit

--- WEBSITE_BAN_GIAY/js/cart.js
+++ WEBSITE_BAN_GIAY/js/cart.js
@@ -7,56 +7,46 @@ button.addEventListener("click",function(event){{
     var productImg = product.querySelector("img").src
     var productName = product.querySelector("H1").innerText
     var productPrice = product.querySelector("span").innerText
-   
     addcart(productPrice,productImg,productName)
-
 }})
-
 })
-function addcart(productPrice,productImg,productName) {
+
+function addcart(productPrice,productImg,productName){
     var addtr = document.createElement("tr")
     var cartItem = document.querySelectorAll("tbody tr")
-    for (var i=0;i<cartItem.length;i++){
+    for(var i=0;i<cartItem.length;i++){
         var productT = document.querySelectorAll(".title")
-        if (productT[i].innerHTML==productName){
-            alert("Sản phẩm của bạn đã có trong giỏ hàng")
+        if(productT[i].innerHTML == productName){
+            alert("Sản phẩm của bạn đã có trong giỏ hàng!")
             return
         }
     }
-    var trcontent = '<tr><td style="display: flex;align-items: center;"><img style="width:70px" src="'+productImg+'" alt=""><span class="title">'+productName+'</span></td><td><p><span class="prices">'+productPrice+'</span><sup>đ</sup></p></td><td><input style="width: 30px;outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;"><span class="cart-delete">Xóa</span></td></tr>'
+    var trcontent = '<tr><td style="display: flex; align-items: center;"><img src="'+productImg+'" width="70px" alt=""><span class="title">'+productName+'</span></td><td><select><option>36</option><option>37</option><option>38</option><option>39</option><option>40</option><option>41</option><option>42</option><option>43</option><option>44</option><option>45</option></select></td><td><p><span class="prices">'+productPrice+'</span><sup>đ</sup></p></td><td><input style="width: 40px;outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;"><span class="cart-delete">Xóa</span></td></tr>'
     addtr.innerHTML = trcontent
     var cartTable = document.querySelector("tbody")
-
     cartTable.append(addtr)
+
     carttotal()
     deleteCart()
-  
-
 }
-//---------------------totalprice----------------------------
-function carttotal (){
+
+//---tongtien-----
+function carttotal(){
     var cartItem = document.querySelectorAll("tbody tr")
     var totalC = 0
-    // console.log(cartItem.length)
-    for (var i=0;i<cartItem.length;i++){
-        var inputValue =  cartItem[i].querySelector("input").value
-        // console.log(inputValue)
+    for(var i=0;i<cartItem.length;i++){
+        var inputValue = cartItem[i].querySelector("input").value
         var productPrice = cartItem[i].querySelector(".prices").innerHTML
-        // console.log(productPrice)
         totalA = inputValue*productPrice*1000
-        // totalB = totalA.toLocaleString("de-DE")
-        // console.log(totalB)
         totalC = totalC + totalA
-        // totalD =totalC.toLocaleString("de-DE")
     }
     var cartTotalA = document.querySelector(".price-total span")
-    var cartPrice = document.querySelector(".cart-icon span")
-    cartTotalA.innerHTML = totalC.toLocaleString("de-DE")
-    cartPrice.innerHTML = totalC.toLocaleString("de-DE")
-    inputchange ()
-    // console.log(cartTotalA)
+    var cartPrice =document.querySelector(".cart-icon")
+    cartTotalA.innerHTML = totalC.toLocaleString('de-DE')
+    cartPrice.innerHTML = totalC.toLocaleString('de-DE')
+    inputchange()
 }
-//---------------------Deletet cart----------------------------
+//-----------xoa--------
 function deleteCart(){
     var cartItem = document.querySelectorAll("tbody tr")
     for (var i=0;i<cartItem.length;i++){
@@ -65,30 +55,16 @@ function deleteCart(){
             var cartDelete = event.target
             var cartitemR = cartDelete.parentElement.parentElement
             cartitemR.remove()
-            carttotal ()
-            // console.log(cartitemR)
+            carttotal()
         })
-      
     }
 }
-function inputchange () {
+function inputchange(){
     var cartItem = document.querySelectorAll("tbody tr")
     for (var i=0;i<cartItem.length;i++){
         var inputValue = cartItem[i].querySelector("input")
         inputValue.addEventListener("change",function(){
-            carttotal ()
+            carttotal()
         })
-      
     }
 }
-const cartbtn = document.querySelector(".fa-times")
-const cartshow = document.querySelector(".fa-shopping-cart")
-cartshow.addEventListener("click",function(){
-    console.log(cartshow)
-    document.querySelector(".cart").style.right = "0"
-})
-cartbtn.addEventListener("click",function(){
-    console.log(cartshow)
-    document.querySelector(".cart").style.right = "-100%"
-}) 
-                        
--- WEBSITE_BAN_GIAY/js/scroll.js
+++ WEBSITE_BAN_GIAY/js/scroll.js
@@ -1,11 +1,70 @@
-const nav = document.querySelector(".nav");
-    let lastScrollY = window.scrollY;
+const btn = document.querySelectorAll("button")
+console.log(btn)
+btn.forEach(function(button,index){
+button.addEventListener("click",function(event){{
+    var btnItem = event.target
+    var product = btnItem.parentElement
+    var productImg = product.querySelector("img").src
+    var productName = product.querySelector("H1").innerText
+    var productPrice = product.querySelector("span").innerText
+    addcart(productPrice,productImg,productName)
+}})
+})
 
-    window.addEventListener("scroll", () => {
-      if(lastScrollY < window.scrollY) {
-        nav.classList.add("nav--hiden");
-        } else {
-        nav.classList.remove("nav--hiden");
+function addcart(productPrice,productImg,productName){
+    var addtr = document.createElement("tr")
+    var cartItem = document.querySelectorAll("tbody tr")
+    for(var i=0;i<cartItem.length;i++){
+        var productT = document.querySelectorAll(".title")
+        if(productT[i].innerHTML == productName){
+            alert("Sản phẩm của bạn đã có trong giỏ hàng!")
+            return
         }
-        lastScrollY = window.scrollY;
-      })
+    }
+    var trcontent = '<tr><td style="display: flex; align-items: center;"><img src="'+productImg+'" width="70px" alt=""><span class="title">'+productName+'</span></td><td><select><option>36</option><option>37</option><option>38</option><option>39</option><option>40</option><option>41</option><option>42</option><option>43</option><option>44</option><option>45</option></select></td><td><p><span class="prices">'+productPrice+'</span><sup>đ</sup></p></td><td><input style="width: 40px;outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;"><span class="cart-delete">Xóa</span></td></tr>'
+    addtr.innerHTML = trcontent
+    var cartTable = document.querySelector("tbody")
+    cartTable.append(addtr)
+
+    carttotal()
+    deleteCart()
+}
+
+//---tongtien-----
+function carttotal(){
+    var cartItem = document.querySelectorAll("tbody tr")
+    var totalC = 0
+    for(var i=0;i<cartItem.length;i++){
+        var inputValue = cartItem[i].querySelector("input").value
+        var productPrice = cartItem[i].querySelector(".prices").innerHTML
+        totalA = inputValue*productPrice*1000
+        totalC = totalC + totalA
+    }
+    var cartTotalA = document.querySelector(".price-total span")
+    var cartPrice =document.querySelector(".cart-icon")
+    cartTotalA.innerHTML = totalC.toLocaleString('de-DE')
+    cartPrice.innerHTML = totalC.toLocaleString('de-DE')
+    inputchange()
+}
+//-----------xoa--------
+function deleteCart(){
+    var cartItem = document.querySelectorAll("tbody tr")
+    for (var i=0;i<cartItem.length;i++){
+        var productT = document.querySelectorAll(".cart-delete")
+        productT[i].addEventListener("click",function(event){
+            var cartDelete = event.target
+            var cartitemR = cartDelete.parentElement.parentElement
+            cartitemR.remove()
+            carttotal()
+        })
+    }
+}
+function inputchange(){
+    var cartItem = document.querySelectorAll("tbody tr")
+    for (var i=0;i<cartItem.length;i++){
+        var inputValue = cartItem[i].querySelector("input")
+        inputValue.addEventListener("change",function(){
+            carttotal()
+        })
+    }
+}
